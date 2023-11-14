b782681844e5a12e683a096ef8ce7dbd60840498
allan-yego <allanyego05@gmail.com>
2023-11-14 06:21:51 +0300

    fix

---
+++ flaskr/config.py
@@ -0,0 +1,6 @@
+import os
+
+APP_SECRET = b'VLt3MBeq214Mt2Nn-B0y7_JtEthWBTgG-Ihim_VilvI='
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+MONGO_URI = os.environ.get('MONGO_URI')
